088ab572b5878225e8d484bc693529d0d5afca50
Jeffery Zhang <zhenyu051988@gmail.com>
2020-03-11 00:49:42 -0700

    replace request with axios

--- server/index.js
+++ server/index.js
@@ -1,36 +1,36 @@
 /* eslint consistent-return:0 import/order:0 */
 
-const express = require('express');
-const logger = require('./logger');
+const express = require("express");
+const logger = require("./logger");
 // for spotify auth
-const cors = require('cors');
-const querystring = require('querystring');
-const cookieParser = require('cookie-parser');
-const request = require('request');
+const cors = require("cors");
+const querystring = require("querystring");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
 const {
   client_id,
-  client_secret,
-} = require('./../credentials/client_credentials');
-const redirect_uri = 'http://localhost:8000/callback';
+  client_secret
+} = require("./../credentials/client_credentials");
+const redirect_uri = "http://localhost:8000/callback";
 
 // for graphql
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema')
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
 
-const argv = require('./argv');
-const port = require('./port');
-const isDev = process.env.NODE_ENV !== 'production';
+const argv = require("./argv");
+const port = require("./port");
+const isDev = process.env.NODE_ENV !== "production";
 const ngrok =
   (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
-    ? require('ngrok')
+    ? require("ngrok")
     : false;
-const { resolve } = require('path');
+const { resolve } = require("path");
 
 // generate cookie
 const generateRandomString = function(length) {
-  let text = '';
+  let text = "";
   let possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (let i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -39,7 +39,7 @@ const generateRandomString = function(length) {
 };
 
 // init the cookie key
-const stateKey = 'spotify_auth_state';
+const stateKey = "spotify_auth_state";
 
 // create a express app
 const app = express();
@@ -47,140 +47,116 @@ const app = express();
 // ----------- middle ware ----------
 // serve static files such as images, CSS files, and JavaScript files
 app
-  .use(express.static(__dirname + '/public'))
+  .use(express.static(__dirname + "/public"))
   .use(cors())
   .use(cookieParser());
 
 // use graphql middle ware
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: true,
-  }),
+    graphiql: true
+  })
 );
 
 // ------------ routing ------------
 /*The first call is the service ‘/authorize’ endpoint, passing to it the client ID, scopes, and redirect URI. 
 This is the call that starts the process of authenticating to user and gets the user’s authorization to access data. */
-app.get('/login', function(req, res) {
+app.get("/login", function(req, res) {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   // your application requests authorization, scope means permission from spotify, check document
-  const scope = 'user-read-private user-read-email';
+  const scope = "user-read-private user-read-email user-read-playback-state";
 
   res.redirect(
-    'https://accounts.spotify.com/authorize?' +
+    "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
-        response_type: 'code',
+        response_type: "code",
         client_id: client_id,
         scope: scope,
         redirect_uri: redirect_uri,
-        state: state,
-      }),
+        state: state
+      })
   );
 });
 
 /*The second call is to the Spotify Accounts Service ‘/api/token’ endpoint, 
 passing to it the authorization code returned by the first call and the client secret key. 
 This call returns an access token and also a refresh token. */
-app.get('/callback', function(req, res) {
+app.get("/callback", function(req, res) {
   // your application requests refresh and access tokens
   // after checking the state parameter
 
-  const code = req.query.code || null;
-  const state = req.query.state || null;
-  const storedState = req.cookies ? req.cookies[stateKey] : null;
+  let code = req.query.code || null;
+  let state = req.query.state || null;
+  let storedState = req.cookies ? req.cookies[stateKey] : null;
 
   if (state === null || state !== storedState) {
-    res.redirect(
-      '/#' +
-        querystring.stringify({
-          error: 'state_mismatch',
-        }),
-    );
+    res.redirect("/#" + querystring.stringify({ error: "state_mismatch" }));
   } else {
     res.clearCookie(stateKey);
-    const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code',
-      },
-      headers: {
-        Authorization:
-          'Basic ' +
-          new Buffer(client_id + ':' + client_secret).toString('base64'),
-      },
-      json: true,
+    const params = {
+      client_id,
+      client_secret,
+      redirect_uri,
+      code,
+      grant_type: "authorization_code"
     };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const access_token = body.access_token,
-          refresh_token = body.refresh_token;
-
-        const options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { Authorization: 'Bearer ' + access_token },
-          json: true,
-        };
-
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          // console.log(body);
-        });
-        res.cookie('access_token', access_token)
-        res.cookie('refresh_token', refresh_token)
-        // we can also pass the token to the browser to make requests from there
-        res.redirect(
-          '/#' +
-            querystring.stringify({
-              access_token: access_token,
-              refresh_token: refresh_token,
-            }),
-        );
-      } else {
-        res.redirect(
-          '/#' +
-            querystring.stringify({
-              error: 'invalid_token',
-            }),
-        );
+    axios({
+      method: "post",
+      url: "https://accounts.spotify.com/api/token",
+      params,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
       }
-    });
+    }).then(response => {
+        const access_token = response.data.access_token;
+        const refresh_token = response.data.refresh_token;
+        axios({
+          method: "get",
+          url: "https://api.spotify.com/v1/me",
+          headers: { Authorization: "Bearer " + access_token }
+        }).then(() => {
+            res.redirect(
+              "/#" + querystring.stringify({ access_token, refresh_token })
+            );
+          }).catch(e => {
+            res.redirect(
+              "/#" + querystring.stringify({ error: e.response.data })
+            );
+          });
+      }).catch(e => console.error(e.response.data));
   }
 });
 
 /*The third call, in the code managing requests to ‘/refresh_token’, a refresh token is sent to ‘/api/token’. 
 This will generate a new access token that we can issue when the previous has expired. */
-app.get('/refresh_token', function(req, res) {
+app.get("/refresh_token", function(req, res) {
   // requesting access token from refresh token
   const refresh_token = req.query.refresh_token;
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      Authorization:
-        'Basic ' +
-        new Buffer(client_id + ':' + client_secret).toString('base64'),
-    },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token,
-    },
-    json: true,
+  const params = {
+    client_id,
+    client_secret,
+    grant_type: "refresh_token",
+    refresh_token: refresh_token
   };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const access_token = body.access_token;
-      res.send({
-        access_token: access_token,
-      });
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    params,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
     }
-  });
+  }).then(response => {
+      access_token = response.data.access_token;
+      res.send({
+        access_token: access_token
+      });
+    }).catch(e => {
+      console.error(e.response.data);
+    });
 });
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
@@ -189,12 +165,12 @@ app.get('/refresh_token', function(req, res) {
 // get the intended host and port number, use localhost and port 3000 if not provided
 const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
-const prettyHost = customHost || 'localhost';
+const prettyHost = customHost || "localhost";
 
 // use the gzipped bundle
-app.get('*.js', (req, res, next) => {
-  req.url = req.url + '.gz'; // eslint-disable-line
-  res.set('Content-Encoding', 'gzip');
+app.get("*.js", (req, res, next) => {
+  req.url = req.url + ".gz"; // eslint-disable-line
+  res.set("Content-Encoding", "gzip");
   next();
 });
 
