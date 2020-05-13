adb77c7ed480a1412df7250ede9e4d7ef924fc08
Jeffery Zhang <zhenyu051988@gmail.com>
2020-05-13 00:33:32 -0700

    move connect to spotify sdk to maincontent page

--- client/src/components/content/main-content/MainContent.jsx
+++ client/src/components/content/main-content/MainContent.jsx
@@ -1,12 +1,89 @@
-import React from "react";
+import React, { Component } from "react";
 import "./MainContent.scss";
+// utils
+import { getAccessToken } from "../../../utils/getAccessToken";
 
-const MainContent = () => {
-  return (
-    <div className="main-content-container">
-      <div className="player-container">player</div>
-    </div>
-  );
-};
+class MainContent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: getAccessToken(),
+      deviceId: "",
+      player: null,
+    };
+    this.playerCheckInterval = null;
+  }
+
+  componentDidMount() {
+    if (this.state.token) {
+      // check every second for the player.
+      this.playerCheckInterval = setInterval(() => this.checkForPlayer(), 1000);
+    }
+  }
+
+  checkForPlayer() {
+    const { token } = this.state;
+    if (window.Spotify) {
+      clearInterval(this.playerCheckInterval);
+      // The main constructor for initializing the Web Playback SDK.
+      // It should contain an object with the player name, volume and access token.
+      this.setState({
+        player: new window.Spotify.Player({
+          name: "Jeffrey's Spotify Player",
+          getOAuthToken: (callback) => {
+            callback(token);
+          },
+          volume: 0.5,
+        }),
+      });
+      this.createEventHandlers();
+
+      // finally, connect!
+      // Connect to Web Playback SDK instance to Spotify with the credentials provided during initialization.
+      this.state.player.connect().then((success) => {
+        if (success) {
+          console.log(
+            "The Web Playback SDK successfully connected to Spotify!"
+          );
+        }
+      });
+    }
+  }
+
+  createEventHandlers() {
+    const { player } = this.state;
+    // Create a new event listener in the Web Playback SDK.
+    player.on("initialization_error", (e) => {
+      console.error(e);
+    });
+
+    player.on("account_error", (e) => {
+      console.error(e);
+    });
+    player.on("playback_error", (e) => {
+      console.error(e);
+    });
+
+    // Playback status updates
+    player.on("player_state_changed", (state) => {
+      console.log(state);
+    });
+
+    // Ready
+    player.on("ready", (data) => {
+      let { device_id } = data;
+      this.setState({ deviceId: device_id });
+    });
+  }
+
+  render() {
+    return (
+      <div className="main-content-container">
+        <div className="player-container">player</div>
+      </div>
+    );
+  }
+}
 
 export default MainContent;
--- client/src/pages/home-page/HomePage.jsx
+++ client/src/pages/home-page/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./HomePage.scss";
 // reselect & seletors
 import { createStructuredSelector } from "reselect";
@@ -16,105 +16,25 @@ import Header from "../../components/header/Header";
 import LeftContent from "../../components/content/left-content/LeftContent";
 import RightContent from "../../components/content/right-content/RightContent";
 import MainContent from "../../components/content/main-content/MainContent";
-// utils
-import { getAccessToken } from "../../utils/getAccessToken";
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      myProfile: this.props.myProfile,
-      token: getAccessToken(),
-      deviceId: "",
-      player: null,
-    };
-    this.playerCheckInterval = null;
-  }
+const HomePage = ({ getMyProfile, loginedUser, isLoading }) => {
+  useEffect(() => {
+    getMyProfile();
+  }, [getMyProfile]);
 
-  componentDidMount() {
-    this.props.getMyProfile();
-    if (this.state.token !== "") {
-      this.setState({ loggedIn: true });
-      // check every second for the player.
-      this.playerCheckInterval = setInterval(() => this.checkForPlayer(), 1000);
-    }
-  }
-
-  checkForPlayer() {
-    const { token } = this.state;
-    if (window.Spotify !== null) {
-      clearInterval(this.playerCheckInterval);
-      // The main constructor for initializing the Web Playback SDK.
-      // It should contain an object with the player name, volume and access token.
-      this.setState({
-        player: new window.Spotify.Player({
-          name: "Jeffrey's Spotify Player",
-          getOAuthToken: (callback) => {
-            callback(token);
-          },
-          volume: 0.5,
-        }),
-      });
-      this.createEventHandlers();
-
-      // finally, connect!
-      // Connect to Web Playback SDK instance to Spotify with the credentials provided during initialization.
-      this.state.player.connect().then((success) => {
-        if (success) {
-          console.log(
-            "The Web Playback SDK successfully connected to Spotify!"
-          );
-        }
-      });
-      console.log("something here", this.state.player);
-    }
-  }
-
-  createEventHandlers() {
-    const { player } = this.state;
-    // Create a new event listener in the Web Playback SDK.
-    player.on("initialization_error", (e) => {
-      console.error(e);
-    });
-    // player.on("authentication_error", (e) => {
-    //   console.error(e);
-    //   this.setState({ loggedIn: false });
-    // });
-    player.on("account_error", (e) => {
-      console.error(e);
-    });
-    player.on("playback_error", (e) => {
-      console.error(e);
-    });
-
-    // Playback status updates
-    player.on("player_state_changed", (state) => {
-      console.log(state);
-    });
-
-    // Ready
-    player.on("ready", (data) => {
-      let { device_id } = data;
-      console.log("Let the music play on!", this);
-      this.setState({ deviceId: device_id });
-    });
-  }
-
-  render() {
-    return this.props.isLoading ? (
-      <LoadingSpinner asOverlay />
-    ) : (
-      <div className="home-container">
-        <Header loginedUser={this.props.loginedUser} />
-        <div className="content-container">
-          <LeftContent />
-          <MainContent />
-          <RightContent />
-        </div>
+  return isLoading ? (
+    <LoadingSpinner asOverlay />
+  ) : (
+    <div className="home-container">
+      <Header loginedUser={loginedUser} />
+      <div className="content-container">
+        <LeftContent />
+        <MainContent />
+        <RightContent />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   loginedUser: selectMyProfileLoginedUser,
