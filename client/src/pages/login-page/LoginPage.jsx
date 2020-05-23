7fcb51a543905568e530c3fb35bd057b2d0746af
Jeffery Zhang <zhenyu051988@gmail.com>
2020-05-22 21:03:18 -0700

    add production path for login

--- client/src/pages/login-page/LoginPage.jsx
+++ client/src/pages/login-page/LoginPage.jsx
@@ -5,7 +5,14 @@ import { ReactComponent as Logo } from "../../assets/login-icon.svg";
 const LoginPage = () => {
   return (
     <div className="login-button-container">
-      <a href="http://localhost:8000/login" className="login-button">
+      <a
+        href={
+          process.env.NODE_ENV === "production"
+            ? "http://localhost:8000/login"
+            : "https://snowpea.herokuapp.com/login"
+        }
+        className="login-button"
+      >
         <Logo className="logo" />
         Login With Spotify
       </a>
